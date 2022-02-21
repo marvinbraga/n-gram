f1772280da0928958089b7052b29b38201e62a64
Marcus Braga <mvbraga@gmail.com>
2022-02-21 02:10:13 -0300

    Adjusts

--- source/oop_code.py
+++ source/oop_code.py
@@ -2,13 +2,7 @@
 """
 Prova "Desenvolvedor Jovens Talentos"
 Desafio para Engenheiro de Software.
-
 Marcus Vinicius Braga, 2022.
-
-Faça um programa simples e legível, em qualquer linguagem de programação, mas sem uso
-de bibliotecas "prontas" ou "externas", que:
-— leia um arquivo de texto e receba um parâmetro N > 0;
-— Gere uma saída do arquivo com as frequências ordenadas dos N-gramas.
 """
 import sys
 
