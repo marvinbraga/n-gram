c9ad655c6eaed064e113e8cfe6c2fe9511e069b8
Marcus Braga <mvbraga@gmail.com>
2022-02-22 01:03:14 -0300

    Create tests to oop-code. Close#15

--- source/tests/test_argv.py
+++ source/tests/test_argv.py
@@ -5,33 +5,40 @@ N-GRAM Challenger - Test Software Engineer.
 Marcus Vinicius Braga, 2022.
 Todos os direitos reservados.
 """
+import pytest
+
 from source.utils.argv import ParserArgv
 
 
+@pytest.mark.argv
 def test_argv_one_invalid_param():
     argv = ["./source/texto.txt"]
     result = ParserArgv(argv).parse()
     assert not result.get("source_file") and not result.get("n_gram")
 
 
+@pytest.mark.argv
 def test_argv_two_invalid_params():
     argv = ["./source/texto.txt", "1"]
     result = ParserArgv(argv).parse()
     assert not result.get("source_file") and not result.get("n_gram")
 
 
+@pytest.mark.argv
 def test_argv_source_file_valid():
     argv = ["source_file=./source/texto.txt"]
     result = ParserArgv(argv).parse()
     assert result.get("source_file") and not result.get("n_gram")
 
 
+@pytest.mark.argv
 def test_argv_n_gram_valid():
     argv = ["n_gram=1"]
     result = ParserArgv(argv).parse()
     assert not result.get("source_file") and result.get("n_gram")
 
 
+@pytest.mark.argv
 def test_argv_valid_params():
     argv = ["source_file=./source/texto.txt", "n_gram=1"]
     result = ParserArgv(argv).parse()
--- source/tests/test_oop_code.py
+++ source/tests/test_oop_code.py
@@ -13,21 +13,50 @@ from source.oop_code import Serializer, Loader, Fitter
 
 
 @pytest.fixture
-def source():
+def test_loader():
     source_file = os.path.normpath("./source/texto.txt")
-    return Serializer(lines=Loader(source_file=source_file).execute().lines).process()
+    return Loader(source_file=source_file).execute()
 
 
+@pytest.fixture
+def test_source(test_loader):
+    return Serializer(lines=test_loader.lines).process()
+
+
+@pytest.mark.oop_code
 @pytest.mark.initialization
-def tests_load_source_file(source):
-    assert len(source.words) > 0
+def tests_loader(test_loader):
+    assert len(test_loader.lines) > 0
 
 
+@pytest.mark.oop_code
+@pytest.mark.initialization
+def tests_load_source_file(test_source):
+    assert len(test_source.words) > 0
+
+
+@pytest.mark.oop_code
 @pytest.mark.calculate
-def tests_calculate_n_gram1(source):
-    assert Fitter(n_gram=1, words=source.words).count().result
+def tests_calculate_n_gram1(test_source):
+    output = Fitter(n_gram=1, words=test_source.words).count().result
+    assert output
+    assert len(output) == 16
+    assert output["cada"] == 8
+    assert output["um"] == 8
+    assert output["porque"] == 3
+    assert output["de"] == 2
+    assert output["vai"] == 1
 
 
+@pytest.mark.oop_code
 @pytest.mark.calculate
-def tests_calculate_n_gram2(source):
-    assert Fitter(n_gram=2, words=source.words).count().result
+def tests_calculate_n_gram2(test_source):
+    output = Fitter(n_gram=2, words=test_source.words).count().result
+    assert output
+    assert len(output) == 24
+    assert output["cada um"] == 8
+    assert output["a casa"] == 2
+    assert output["é porque"] == 2
+    assert output["não fosse"] == 1
+    assert output["vá lá"] == 1
+    assert output["queria que"] == 1
